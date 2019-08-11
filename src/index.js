dc28315e61afac104a3e41ea9e8f297404fd5ec4
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-08-11 20:41:15 -0300

    Render h1 Hello World

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
