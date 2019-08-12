37572de57a228229cb29012931153ab6ed804e11
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-08-11 22:15:48 -0300

    Duplicate li more 3 times

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -25,6 +25,57 @@ export default function Main({ match }) {
             </button>
           </div>
         </li>
+
+        <li>
+          <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Programador e cantor sertanejo nos tempos livres.</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Programador e cantor sertanejo nos tempos livres.</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Programador e cantor sertanejo nos tempos livres.</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
       </ul>
     </div>
   );
