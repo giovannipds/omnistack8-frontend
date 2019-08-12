3db3b0768ebaeb0507d95322e2e462e3d9bb2181
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-08-11 21:43:05 -0300

    Show some card content to devs at Main

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
+import logo from '../assets/logo.svg';
+
 export default function Main({ match }) {
-  return <h1>{match.params.id}</h1>
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Tindev" />
+      <ul>
+        <li>
+          <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Programador e cantor sertanejo nos tempos livres.</p>
+          </footer>
+        </li>
+      </ul>
+    </div>
+  );
 }
