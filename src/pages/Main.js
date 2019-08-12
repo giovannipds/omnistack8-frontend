784607bb5ac9e4969ab36a55dffb44c4173d797c
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-08-11 23:01:05 -0300

    Handle empty users list and style its message

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -39,26 +39,30 @@ export default function Main({ match }) {
   return (
     <div className="main-container">
       <img src={logo} alt="Tindev" />
-      <ul>
-        {users.map(user => (
-          <li key={user._id}>
-            <img src={user.avatar} alt={user.name} />
-            <footer>
-              <strong>{user.name}</strong>
-              <p>{user.bio}</p>
-            </footer>
+      { users.length > 0 ? (
+        <ul>
+          {users.map(user => (
+            <li key={user._id}>
+              <img src={user.avatar} alt={user.name} />
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
 
-            <div className="buttons">
-              <button type="button">
-                <img src={dislike} alt="Dislike" onClick={() => handleDislike(user._id)} />
-              </button>
-              <button type="button">
-                <img src={like} alt="Like" onClick={() => handleLike(user._id)} />
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+              <div className="buttons">
+                <button type="button">
+                  <img src={dislike} alt="Dislike" onClick={() => handleDislike(user._id)} />
+                </button>
+                <button type="button">
+                  <img src={like} alt="Like" onClick={() => handleLike(user._id)} />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">Acabou :(</div>
+      )}
     </div>
   );
 }
