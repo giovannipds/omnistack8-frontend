0d41eb1925f676af90d6f567d3ef3e7c82161d33
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-08-18 12:10:38 -0300

    Make frontend send a msg after 3 seconds

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -28,6 +28,12 @@ export default function Main({ match }) {
 
   useEffect(() => {
     const socket = io('http://localhost:3333');
+
+    setTimeout(() => {
+      socket.emit('hello', {
+        message: 'Hello World'
+      })
+    }, 3000);
   }, [match.params.id]);
 
   async function handleLike(id) {
